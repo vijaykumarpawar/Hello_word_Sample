6adfcf474633f97de01fe53865bdb3205379cc0b
vijaykumarpawar <vijaykumarpawar@users.noreply.github.com>
2018-01-16 21:08:11 +0530

    sample code
    sample code uploaded

---
+++ Hello_World/src/com/test/test.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class test extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doget menthod");
+		super.doGet(req, resp);
+	}
+	
+
+}
